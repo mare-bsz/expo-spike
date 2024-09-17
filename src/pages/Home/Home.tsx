6a64768adcfaf3c6719da71623e8efa735b56a7a
Patrick Rothweil <patrick.rothweil@seitenbau.com>
2024-09-17 14:46:20 +0200

    chore: clean up Header and Home labels

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import Header from '../../components/Header/Header';
 import './Home.scss';
 import Search from '../../components/Search/Search';
 
 const Home: React.FC = () => {
-  const { t } = useTranslation('home');
-
   return (
     <div>
       <Header />
