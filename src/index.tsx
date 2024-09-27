1bd74ae4ae2207385214c3713d963fb8f90148e1
Patrick Rothweil <patrick.rothweil@seitenbau.com>
2024-09-27 16:12:13 +0200

    refactor: rename i18n folder to translations

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './i18n/i18n';
+import './translations/i18n';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
