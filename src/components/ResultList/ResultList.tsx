7c8e2a7a27f0bb63ed73bf0d89701e57e0ff68fa
Patrick Rothweil <patrick.rothweil@seitenbau.com>
2024-10-04 15:12:29 +0200

    feature(Image): add modifiers; adjust markup and styling

--- src/components/ResultList/ResultList.tsx
+++ src/components/ResultList/ResultList.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Record } from '../../types/types';
 import './ResultList.scss';
-import Image from '../../shared/Image/Image';
+import Image, { ImageModifier } from '../../shared/Image/Image';
 
 type ResultListProps = {
   records: Record[];
@@ -14,16 +14,22 @@ const ResultList: React.FC<ResultListProps> = ({ records, searchTerm }) => {
     <ul className="result-list">
       {records.map((record, index) => (
         <li key={index} className="result-list__item">
-          <Image
-            imdasId={record.imdasid}
-            title={record.anzeigetitel}
-            width={400}
-          />
           <Link
+            className="result-list__link"
             to={`/detail/${record.imdasid}`}
             state={{ record, searchTerm, records }}
           >
-            {record.anzeigetitel}
+            <>
+              <div className="result-list__item-image">
+                <Image
+                  imdasId={record.imdasid}
+                  title={record.anzeigetitel}
+                  width={300} // fetched images are quite blurry under 300px
+                  modifier={ImageModifier.COVER}
+                />
+              </div>
+              <p className="result-list__item-title">{record.anzeigetitel}</p>
+            </>
           </Link>
         </li>
       ))}
--- src/shared/Image/Image.tsx
+++ src/shared/Image/Image.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import './Image.scss';
+import classNames from 'classnames';
+
+export enum ImageModifier {
+  COVER = 'cover',
+  INVISIBLE = 'invisible',
+}
 
 type ImageProps = {
   imdasId: string;
   title: string;
   width: number;
+  modifier?: ImageModifier;
 };
 
-const Image: React.FC<ImageProps> = ({ imdasId, title, width }) => {
+const Image: React.FC<ImageProps> = ({ imdasId, modifier, title, width }) => {
   const { t } = useTranslation('image');
   const src = `/sbspike/image?id=${imdasId}&width=${width}`;
   const alt = `${t('altText')} ${title}`;
+  const [isInvisible, setIsInvisible] = React.useState(false);
 
-  return <img src={src} alt={alt} />;
+  return (
+    <img
+      className={classNames('image', {
+        [`image--${modifier}`]: Boolean(modifier),
+        [`image--${ImageModifier.INVISIBLE}`]: isInvisible,
+      })}
+      src={src}
+      alt={alt}
+      width={width}
+      onError={() => {
+        // if there is no image (404), just show empty space
+        setIsInvisible(true);
+      }}
+    />
+  );
 };
 
 export default Image;
